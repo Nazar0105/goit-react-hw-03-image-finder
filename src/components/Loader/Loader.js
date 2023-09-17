89fd341372a5c9d5704e390f9562e329558c108e
Nazar0105 <nazar01051999@gmail.com>
2023-09-17 18:44:34 +0300

    Loader)))))))

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,24 +1,26 @@
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Loader.module.css';
 
-const Loader = ({ visible, height, width, ariaLabel, wrapperClass, wrapperStyle }) => {
+const Loader = ({ visible }) => {
   if (!visible) {
     return null;
   }
 
   return (
-    <div className={`${styles.LoaderWrapper} ${wrapperClass}`} style={wrapperStyle}>
+    <div className={styles.LoaderWrapper}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
-        width={width}
-        height={height}
-        aria-label={ariaLabel}
+        aria-label="Loader"
         role="img"
         className={styles.Loader}
       >
-        <circle cx="50" cy="50" r="45" />
+        <circle cx="50" cy="50" r="45" className={styles.circle1} />
+        <circle cx="50" cy="50" r="35" className={styles.circle2} />
+        <circle cx="50" cy="50" r="25" className={styles.circle3} />
+        <circle cx="50" cy="50" r="15" className={styles.circle4} />
       </svg>
     </div>
   );
@@ -26,20 +28,8 @@ const Loader = ({ visible, height, width, ariaLabel, wrapperClass, wrapperStyle 
 
 Loader.propTypes = {
   visible: PropTypes.bool.isRequired,
-  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  ariaLabel: PropTypes.string,
-  wrapperClass: PropTypes.string,
-  wrapperStyle: PropTypes.object,
-};
-
-Loader.defaultProps = {
-  height: 80,
-  width: 80,
-  ariaLabel: 'Loader',
-  wrapperClass: '',
-  wrapperStyle: {},
 };
 
 export default Loader;
+
 
